186bfbb0a2c5fcf59fe0169af79b03ad603ac2da
koksibes <koksibes@gmail.com>
2021-04-28 16:24:08 +0300

    initial commit

--- homeworkless4.py
+++ homeworkless4.py
@@ -1,3 +1,5 @@
+import random
+
 """
 Task 1
 
@@ -6,15 +8,15 @@ Write a program that generates a random number between 1 and 10 and lets the use
 The result should be sent back to the user via a print statement.
 
 """
-"""
-import random
+
+
 guessednumber = random.randint(0, 10)
 trynumber = int(input("Try to guess my number "))
 if guessednumber == trynumber:
     print(f"Yes my nymber is {guessednumber}")
 else:
     print(f"No my nymber is {guessednumber}, maybe next time")
-"""
+
 """
 Task 2
 
@@ -24,11 +26,11 @@ Write a program that takes your name as input, and then your age as input and gr
 
 “Hello <name>, on your next birthday you’ll be <age+1> years”   
 """
-"""
+
 name = input("What's your name? ")
 age = input("How old are you? ")
 print(f"Hello {name.capitalize()}, on your next birthday you’ll be {int(age) + 1} years")
-"""
+
 """
 Task 3
 
@@ -44,9 +46,12 @@ so it should print 5 random strings(words) that combine characters
 
 Tips: Use random module to get random char from string)
 """
-import random
-c=random.choice("hello")
-print(c)
+a = list("hello")
+count = 0
+while count < 5:
+    count += 1
+    random.shuffle(a)
+    print("".join(a))
 """
 Task 4
 
@@ -55,4 +60,25 @@ The math quiz program
 Write a program that asks the answer for a mathematical expression, checks whether the user is right or wrong, 
 and then responds with a message accordingly.
 """
-#
+
+while True:
+    num1 = random.randint(0, 100)
+    num2 = random.randint(0, 100)
+    answer = input(f"Inert answer {num1} + {num2} :")
+    if answer == str(num1 + num2):
+        print("Great, catch your cookie ")
+        break
+    else:
+        print("Try again.")
+
+"""
+*****
+"""
+linechess = 0
+while linechess < 8:
+    if linechess == 0 or linechess % 2 == 0:
+        print(" W  B " * 4)
+        linechess += 1
+    else:
+        print(" B  W " * 4)
+        linechess += 1
